d0ffcc4abd151314e26b7c8e25a2fac68f8ba662
mrmtsu <81202muramatsu@gmail.com>
2021-12-25 15:53:55 +0900

    ✨ カウンターを設置しuseStateでアップする

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import { Headline } from "src/components/Headline";
 import { Links } from "src/components/Links";
 import classes from "src/components/Main/Main.module.css";
 
 export function Main(props) {
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <main className={classes.main}>
       <Headline page={props.page}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,17 @@
 import Head from "next/head";
-import { useCallback } from "react";
+import { useState } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   return (
     <div className={styles.container}>
@@ -21,9 +20,9 @@ export default function Home() {
       </Head>
 
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
 
       <Footer />
