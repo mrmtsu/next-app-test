ecbc7e6cdb658c043926868abef88b2204c8ed52
mrmtsu <81202muramatsu@gmail.com>
2022-01-02 11:31:15 +0900

    🐛 パス修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,18 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
-const About = (props) => {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
-
       <Header />
-
-      {props.isShow ? <h2>{props.doubleCount}</h2> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisPlay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ol>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ol>
-
-      <Main page="about" />
-
-      <Footer />
+      <h1>Next.js</h1>
+      <p>JSONPlaceholder API</p>
     </div>
   );
 };
 
-export default About;
+export default Index;
