cfe46bf7be77051474abd9ce1e5c5e9dc4902718
mrmtsu <81202muramatsu@gmail.com>
2022-01-02 11:23:31 +0900

    ✨ fetchの共通化

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -1,18 +1,12 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
 import { Header } from "src/components/Header";
+import { Post } from "src/components/Post";
 import styles from "src/styles/Home.module.css";
 
 const PostId = () => {
-  const router = useRouter();
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   );
 };
